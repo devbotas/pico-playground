9a396e15041e6ea5f9a1e99b56e881d56dbaf657
devbotas <simon@devbot.lt>
2022-08-16 14:51:25 +0300

    Adding simple blinky for pico_w.

--- PicoFreeRTOS/Code/main.c
+++ PicoFreeRTOS/Code/main.c
@@ -1,4 +1,18 @@
-int main(void)
-{
-        
+#include "pico/stdlib.h"
+#include "pico/cyw43_arch.h"
+
+
+int main() {
+    stdio_init_all();
+    if (cyw43_arch_init()) {
+        printf("WiFi init failed");
+        return -1;
+    }
+    while (true) {
+        printf("bybis");
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+        sleep_ms(250);
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+        sleep_ms(1250);
+    }
 }
