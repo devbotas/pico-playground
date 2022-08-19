3d4fa4146e635cf87bce468484ae605eca4be9f5
devbotas <simon@devbot.lt>
2022-08-19 13:39:41 +0300

    Basic printf sample works.

--- PicoFreeRTOS/Basic/main.c
+++ PicoFreeRTOS/Basic/main.c
@@ -1,8 +1,11 @@
 #include "pico/stdlib.h"
+#include "pico/cyw43_arch.h"
 
 int main() {
+    stdio_init_all();
+    
     while (true) {
-        printf("Sveiks, Girdauskai!");
+        printf("Sveiks, Girdauskai!\n");
         sleep_ms(1000);
     }
 }
